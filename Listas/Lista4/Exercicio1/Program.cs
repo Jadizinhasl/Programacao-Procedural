377084ee6241b0df67c666117169662fe7491346
jadizinhasl <jadi6486@gmail.com>
2024-03-13 21:18:38 -0300

    Laços de repetição aninhados

---
+++ Listas/Lista4/Exercicio1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] agrs)
+    {
+        for (int fator = 1; fator <= 10; fator++)
+        {
+            for (int contador = 1; contador <= 10; contador++)
+            {
+                Console.WriteLine(fator + " X " + contador + " = " + fator * contador);
+            }
+            Console.WriteLine();
+        }
+    }
+}
