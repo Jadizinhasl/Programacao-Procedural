e304baafe506e583dec7507caff2b546639405d1
jadizinhasl <jadi6486@gmail.com>
2024-04-09 20:02:17 -0300

    Structs

---
+++ Listas/Lista8/Ex1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+struct Pizza
+{
+    public int id;
+    public string sabor;
+    public double preco;
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Pizza pizza1;
+        pizza1.id = Convert.ToInt32(Console.ReadLine());
+        pizza1.sabor = Console.ReadLine();
+        pizza1.preco = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine(pizza1.id);
+        Console.WriteLine(pizza1.sabor);
+        Console.WriteLine(pizza1.preco);
+    }
+}
---
+++ Listas/Lista8/Ex2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+struct Produto
+{
+    public int id;
+    public string nome;
+    public double preco;
+    public bool disponivelEmEstoque;
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Produto[] produtos = new Produto[4];
+
+        for(int i = 0; i < produtos.Length; i++)
+        {
+            produtos[i].id = Convert.ToInt32(Console.ReadLine());
+            produtos[i].nome = Console.ReadLine();
+            produtos[i].preco = Convert.ToDouble(Console.ReadLine());
+            produtos[i].disponivelEmEstoque = Convert.ToBoolean(Console.ReadLine());
+        }
+
+            //usei ToString porque não tava aceitando no oneCompiler, por causa do zero depois da virgula
+            
+        for(int i = 0; i < produtos.Length; i++)
+        {
+            if(produtos[i].disponivelEmEstoque)
+            {
+                Console.WriteLine(produtos[i].nome);
+                Console.WriteLine(produtos[i].preco);
+            }
+        }
+    }
+}
